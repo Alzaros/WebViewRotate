4cd2175c781d353003dcb7c4dec0682e81445901
FORESTIER Alban <alban.forestier.ext@herve-thermique.com>
2021-03-12 10:16:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/appliweb/page1.java
@@ -0,0 +1,39 @@
+package com.example.appliweb;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class page1 extends Activity {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page1xml);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://prtg.groupeherve.com/public/1.htm");
+
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setSupportMultipleWindows(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(page1.this, page2.class));
+                finish();
+            }
+        }, secondsDelayed * 60000);
+    }
+}
---
+++ app/src/main/java/com/example/appliweb/page2.java
@@ -0,0 +1,38 @@
+package com.example.appliweb;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class page2 extends Activity {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page2xml);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        WebView webView2 = findViewById(R.id.webview2);
+        webView2.setWebViewClient(new WebViewClient());
+        webView2.loadUrl("http://prtg.groupeherve.com:81/2.htm");
+
+        WebSettings webSettings = webView2.getSettings();
+        webView2.getSettings().setSupportMultipleWindows(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(page2.this, page1.class));
+                finish();
+            }
+        }, secondsDelayed * 60000);
+    }
+}
